25e631b71680c6bd19ba2f4b4ebae2cc8b8d32d7
Wilker Gandra <wilkerbruno@users.noreply.github.com>
2026-02-24 19:37:13 -0300

    pagamentos
    Corrigindo a pagina de pagamentos

--- app.py
+++ app.py
@@ -1505,4 +1505,4 @@ if __name__ == '__main__':
     print('=' * 60 + '\n')
 
     os.makedirs('logs', exist_ok=True)
-    #app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
